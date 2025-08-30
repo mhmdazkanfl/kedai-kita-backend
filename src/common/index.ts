6a02319af6af02de532fe9f7ea2955fd7234e6b3
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-08-30 16:23:53 +0700

    fix: wrong type export

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,3 +1,4 @@
-export { commonModel, createResponse, Response } from './model'
+export { commonModel, createResponse } from './model'
+export type { Response } from './model'
 export { ResponseStatus } from './enum'
 export type { ResponseStatusType } from './enum'
